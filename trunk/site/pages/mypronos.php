743ad49d4ffdeeaefcf4e69f4a0122c183500cc6
j.paroche <j.paroche@8a7d6dee-9698-11de-b6d4-8762f465f3f9>
2009-09-08 23:53:19 +0000

    - Améliorations diverses sur la page de pronostics - Ajout d'une pages qui affiche tous les pronos pour la journée en cours

---
+++ trunk/site/pages/mypronos.php
@@ -0,0 +1,80 @@
+<?php
+require_once('lib/journee.php');
+require_once('lib/match.php');
+require_once('lib/joueur.php');
+require_once('lib/prono.php');
+
+$futures_journees = journee_get_next($all = true);
+if(mysql_num_rows($futures_journees) > 1) {
+	echo '<p>Journée : ';
+	while($nextj = mysql_fetch_assoc($futures_journees))
+		echo '<strong><a href="?p=mypronos&amp;journee='.$nextj['id'].'">'.$nextj['numero'].'</a></strong> ';
+	echo '<br /><br />';
+}
+
+if(isset($_GET['journee'])) {
+	$idjournee = intval($_GET['journee']);
+	if(!journee_exists($idjournee))
+		echo '<span class="error">La journée demandée n\'existe pas</span>';
+	elseif(!journee_has_match($idjournee))
+		echo '<span class="error">Aucun match n\'est enregistré pour la journée demandée</span>';
+	else {
+		$journee = journee_get_by_id($idjournee);
+		echo '<p class="strong">Mes pronostics pour la '.display_number($journee['numero']).' journée (premier match le '.time_to_str($journee['date']).')</p>';
+	}	
+}
+else {
+	$journee = mysql_fetch_assoc(journee_get_next());
+	$idjournee = $journee['id'];
+	if(!journee_has_match($idjournee))
+		echo '<span class="error">Aucun match n\'est enregistré pour la prochaine journée</span>';
+	else
+		echo '<p class="strong">Mes pronostics pour la '.display_number($journee['numero']).' journée (premier match le '.time_to_str($journee['date']).')</p>';
+}
+
+if(isset($_POST['submit_pronos'])) {
+	$display = array('error' => '', 'success' => '');
+	foreach($_POST as $key => $score) {
+		$name = explode('_',$key);
+		if($name[0] == 'match' && $score != '') {
+			if(valid_score($score)) {
+				if(prono_exists($name[1], $_SESSION['id']))
+					prono_update($name[1], $_SESSION['id'], $score);
+				else
+					prono_record($name[1], $_SESSION['id'], $score);
+					
+				$display['success'] = '<span class="success">Les scores correctement écrits ont été enregistrés et/ou mis à jour !</span>';
+			}
+			else
+				$display['error'] = '<span class="error">Un ou plusieurs scores n\'ont pas été enregistrés car la syntaxe était incorrecte</span>';
+		}
+	}
+	echo $display['success'];
+	echo $display['error'];
+}
+
+
+
+$matchs = match_get_by_journee($idjournee);
+if(mysql_num_rows($matchs)) {
+?>
+<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+	<?php
+	while($match = mysql_fetch_assoc($matchs)) {
+		$value = prono_exists($match['id'], $_SESSION['id']) ? prono_get_score($match['id'], $_SESSION['id']) : '';
+		echo '<p><input type="text" name="match_'.$match['id'].'" value="'.$value.'" size="4" />&nbsp;&nbsp;'.$match['equipe1'].' - '.$match['equipe2'].'</p>';
+	}
+	?>
+	<br />
+	<p>
+		<input type="submit" name="submit_pronos" id="submit_pronos" value="Valider mes pronostics" />
+	</p>	
+</form>
+<p style="font-size:10px;">
+Note :<br />
+Les scores doivent être au format "Score1-Score2" (exemples : 3-2, 0-1, 5-1...)<br />
+Vous n'êtes pas obligé de tout remplir en une fois. Vous pouvez modifier vos pronostics tant que le premier match de la journée n'a pas commencé.
+</p>
+<?php
+}
+
