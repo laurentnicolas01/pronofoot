c6b218772ee991cfa090b4c386042f2ac803d6a0
j.paroche <j.paroche@8a7d6dee-9698-11de-b6d4-8762f465f3f9>
2009-09-21 20:36:41 +0000

    - Gestion de l'affichage du classement trié par points ou par moyenne de points par journée.

--- trunk/site/pages/classements.php
+++ trunk/site/pages/classements.php
@@ -2,6 +2,9 @@
 require_once('lib/joueur.php');
 require_once('lib/groupe.php');
 
+$avg = isset($_GET['sort']) && $_GET['sort'] == 'avg';
+$sorted_row = $avg ? 'avg' : 'points';
+$is_asc = isset($_GET['asc']) ? true : false;
 $groups = explode(',', $_SESSION['groups']);
 
 if(isset($_GET['groupe'])) {
@@ -15,8 +18,9 @@ if(isset($_GET['groupe'])) {
 	echo '</p>';
 
 	echo '<h2>Classement pour le groupe '.groupe_get_name($groupe).'</h2><br />';
-
-	$classement = joueur_get_classement($groupe);
+	
+	$classement = joueur_get_classement($groupe,$sorted_row,$is_asc);
+	$uri = 'p=classements&groupe='.$groupe;
 }
 else {
 	echo '<p>Afficher le classement : ';
@@ -24,25 +28,30 @@ else {
 	echo '</p>';
 	echo '<h2>Classement général</h2><br />';
 
-	$classement = joueur_get_classement();
+	$classement = joueur_get_classement(0,$sorted_row,$is_asc);
+	$uri = 'p=classements';
 }
 
-if(mysql_num_rows($classement)) {
-	echo '<table class="table-contain"><th>Position</th><th>Pseudo</th><th>Points</th><th>Journées</th><th>Moyenne par journée</th>';
+if(mysql_num_rows($classement)) {	
+	echo '<table class="table-contain"><th>Position</th><th>Pseudo</th><th>Journées</th>';
+	print_sorted_th('Points', 'points', $sorted_row, $is_asc, $uri);
+	print_sorted_th('Moyenne', 'avg', $sorted_row, $is_asc, $uri);
 	
 	for($i=1 ; $joueur = mysql_fetch_assoc($classement) ; ++$i) {
-		$average = $joueur['journees'] == 0 ? 0 : round($joueur['points']/$joueur['journees'], 1);
-		$position = $i == 1 || $joueur['points'] != $old_score ? $i : $old_position;
+		if($avg)
+			$position = $i == 1 || $joueur['avg'] != $old_value ? $i : $old_position;
+		else
+			$position = $i == 1 || $joueur['points'] != $old_value ? $i : $old_position;
 		
 		echo '<tr>
 		<td>'.$position.'</td>
 		<td>'.$joueur['pseudo'].'</td>
-		<td>'.$joueur['points'].'</td>
 		<td>'.$joueur['journees'].'</td>
-		<td>'.$average.'</td>
+		<td>'.$joueur['points'].'</td>
+		<td>'.round($joueur['avg'], 1).'</td>
 		</tr>';
 		
-		$old_score = $joueur['points'];
+		$old_value = $avg ? $joueur['avg'] : $joueur['points'];
 		$old_position = $position;
 	}
 	
