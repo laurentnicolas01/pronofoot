85b9feb718836422bf35606e274cc4bb8146ed50
j.paroche <j.paroche@8a7d6dee-9698-11de-b6d4-8762f465f3f9>
2009-12-05 18:38:06 +0000

    - Commit de transfert

--- trunk/site/includes/footer.php
+++ trunk/site/includes/footer.php
@@ -1,1 +1,1 @@
-<div id="footer">Valide XHTML & CSS - <a href="?p=contact">Contact</a> - Icons from <a href="http://famfamfam.com">famfamfam</a></div>
+<div id="footer">Valide <a href="http://validator.w3.org/check?uri=referer">XHTML</a> et <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> - <a href="?p=contact">Contact</a> - Icons from <a href="http://famfamfam.com">famfamfam</a></div>
