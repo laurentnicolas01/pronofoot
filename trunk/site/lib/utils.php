2c00790d3e1cbcd2d58677553a1e6c04e7b363be
j.paroche <j.paroche@8a7d6dee-9698-11de-b6d4-8762f465f3f9>
2012-03-04 23:32:46 +0000

    - Développement d'un module d'ajout de matchs par lots à partir d'une url de lequipe.fr - Corrections mineures

--- trunk/site/lib/utils.php
+++ trunk/site/lib/utils.php
@@ -571,3 +571,9 @@ function team_shortname($team) {
 	
 	return $team_init;
 }
+
+function clean_lequipe_teamname($teamname) {
+	$removable_pieces = array('&nbsp;', '(1)', '(2)', '(3)', '(4)', '(5)', '(6)', '(7)', '(8)', '(9)', '(10)', '(11)', '(12)', '(13)', '(14)', '(15)', '(16)', '(17)', '(18)', '(19)', '(20)');
+	$cleaned_teamname = str_replace($removable_pieces, '', $teamname);
+	return trim($cleaned_teamname);
+}
